4a3bc5deadac40c695ae37111b3878595a1302bc
guicgs <etohlif@protonmail.com>
2020-08-01 00:53:20 -0300

    tabela renderizada sem formatação

--- src/pages/CryptoTable.js
+++ src/pages/CryptoTable.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import getCryptoDataFromAPI from '../actions';
 
 class CryptoTable extends Component {
+  componentDidMount() {
+    const { fetchCryptoData } = this.props;
+
+    fetchCryptoData();
+  }
+
   render() {
+    const { data, isFetching } = this.props;
+
+    if (isFetching) return <p>Carregando...</p>;
+
     return (
-      <div>
-        Hello World!
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            {
+              Object.values(data).map((value) => (
+                Object.keys(value).map((key) => key !== 'id' && <th key={key}>{key}</th>)
+              ))[0]
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(data).map(([key, value]) => (
+            <tr>
+              <td key={key}>{key}</td>
+              {Object.entries(value).map(([elKey, elValue]) => elKey !== 'id' && <td>{elValue}</td>)}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default CryptoTable;
+const mapStateToProps = ({ data, isFetching, error }) => ({ data, isFetching, error });
+
+const mapDispatchToProps = (dispatch) => (
+  { fetchCryptoData: () => dispatch(getCryptoDataFromAPI()) }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CryptoTable);
