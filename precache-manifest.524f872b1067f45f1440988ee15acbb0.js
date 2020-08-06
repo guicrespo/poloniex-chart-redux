bf79d1cfb971e613bba3983c3aaa86f77b33186b
guicgs <etohlif@protonmail.com>
2020-08-05 22:19:01 -0300

    Updates

---
+++ precache-manifest.524f872b1067f45f1440988ee15acbb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3f1dcd4c93efbab25433c2b58fca03",
+    "url": "/poloniex-chart-redux/index.html"
+  },
+  {
+    "revision": "5827c1d848b6a935ff9d",
+    "url": "/poloniex-chart-redux/static/css/main.82d294f0.chunk.css"
+  },
+  {
+    "revision": "3cbf1ba5e5e4dba0f8bc",
+    "url": "/poloniex-chart-redux/static/js/2.2fb48375.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poloniex-chart-redux/static/js/2.2fb48375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5827c1d848b6a935ff9d",
+    "url": "/poloniex-chart-redux/static/js/main.e1a81938.chunk.js"
+  },
+  {
+    "revision": "91eda6e0659ac7f4f5fd",
+    "url": "/poloniex-chart-redux/static/js/runtime-main.a8ef594d.js"
+  },
+  {
+    "revision": "a93f391b5600cf0add7f4dea8f2aa423",
+    "url": "/poloniex-chart-redux/static/media/coin_icon.a93f391b.png"
+  },
+  {
+    "revision": "665e9e85fadd18d21e02e8f3622b3a0a",
+    "url": "/poloniex-chart-redux/static/media/linkedin-icon.665e9e85.png"
+  }
+]);
